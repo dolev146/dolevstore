de7f336d1e4204087947226710b00fa6128bc3ec
dolev146 <62290677+dolev146@users.noreply.github.com>
2021-01-20 00:24:40 +0200

    handle 404 on express

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 // imports
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 // mw
-app.use(cors())
-app.use(express.json())
-app.use("/api/customers", require("./routes/customers"))
-app.use("/api/homepage", require("./routes/homepage"))
-app.use("/api/products", require("./routes/products"))
-app.use("/api/items", require("./routes/items"))
-app.use("/api/orders", require("./routes/orders"))
-app.use("/images", express.static("./images"))
+app.use(cors());
+app.use(express.json());
+app.use("/api/customers", require("./routes/customers"));
+app.use("/api/homepage", require("./routes/homepage"));
+app.use("/api/products", require("./routes/products"));
+app.use("/api/items", require("./routes/items"));
+app.use("/api/orders", require("./routes/orders"));
+app.use("/images", express.static("./images"));
 
-app.use("/", express.static("./dist"))
+app.use("/", express.static("./dist"));
 
 app.get("/api", (req, res) => {
-    res.send("welcome to my api")
-})
+  res.send("welcome to my api");
+});
 
 // Serve only the static files form the  directory
 // app.use( express.static(path.join(__dirname + '/dolevstore')))
-
-
-
 
 // app.get('/', (req,res)=> {
 //     res.sendFile(path.join(__dirname+'/dolevstore/index.html'));
 //     });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-})
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
+const port = process.env.PORT || 8080;
+console.log(port);
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-const port = process.env.PORT || 8080
-console.log(port)
 // listening
 app.listen(port, () => {
-    console.log("up and running on " + port),
-    console.log(path.join(__dirname + '/index.html'))
-})
+  console.log("up and running on " + port),
+    console.log(path.join(__dirname + "/index.html"));
+});
